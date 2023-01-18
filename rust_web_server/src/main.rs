937ced04774858a82b422f738b258a55e00ef56f
ljs <qazplm807733@gmail.com>
2023-01-18 14:21:52 +0900

    add single thread web ser

--- rust_web_server/src/main.rs
+++ rust_web_server/src/main.rs
@@ -17,20 +17,47 @@ fn main() {
 
 // 연결 처리 위한 함수
 fn handle_connection(mut stream: TcpStream) {
-    // TcpStream으로부터 데이터를 읽고 출력
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    // TcpStream으로부터 데이터를 읽고 출력 기본 형태
+    // let buf_reader = BufReader::new(&mut stream);
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
 
-    // println!("Request: {:#?}", http_request);
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    // 여러 입력 핸들링 위함
+    // if request_line == "GET / HTTP/1.1" {
+    //     let status_line = "HTTP/1.1 200 OK";
+    //     let contents = fs::read_to_string("hello.html").unwrap();
+    //     let length = contents.len();
+
+    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    //     stream.write_all(response.as_bytes()).unwrap();
+    // } else {
+    //     let status_line = "HTTP/1.1 404 NOT FOUND";
+    //     let contents = fs::read_to_string("404.html").unwrap();
+    //     let length = contents.len();
+
+    //     let response = format! {
+    //         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    //     };
+
+    //     stream.write_all(response.as_bytes()).unwrap();
+    // }
+
+    // 핸들링 코드 간결하게
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
     stream.write_all(response.as_bytes()).unwrap();
 }
