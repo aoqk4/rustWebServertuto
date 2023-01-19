cf57d2548a29cffc7c419499a07f3915e2fc5da2
ljs <qazplm807733@gmail.com>
2023-01-19 20:22:34 +0900

    add thread server

---
+++ rust_web_server/src/lib.rs
@@ -0,0 +1,55 @@
+use crossbeam::channel::{self, Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::{sync::mpsc::channel, thread};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: channel::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = channel::bounded(size);
+
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn excute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
--- rust_web_server/src/main.rs
+++ rust_web_server/src/main.rs
@@ -1,63 +1,85 @@
 // 기본적인 베이스는 TCP를 활용한 바이트 통신과 HTTP를 이용한 요청과 응답
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
 
-use std::fs;
-use std::io::{prelude::*, BufReader};
-use std::net::TcpListener;
-use std::net::TcpStream;
+use rust_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.excute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
-// 연결 처리 위한 함수
-fn handle_connection(mut stream: TcpStream) {
-    // TcpStream으로부터 데이터를 읽고 출력 기본 형태
-    // let buf_reader = BufReader::new(&mut stream);
-    // let http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
+// for multi webserver
 
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // 여러 입력 핸들링 위함
-    // if request_line == "GET / HTTP/1.1" {
-    //     let status_line = "HTTP/1.1 200 OK";
-    //     let contents = fs::read_to_string("hello.html").unwrap();
-    //     let length = contents.len();
-
-    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    //     stream.write_all(response.as_bytes()).unwrap();
-    // } else {
-    //     let status_line = "HTTP/1.1 404 NOT FOUND";
-    //     let contents = fs::read_to_string("404.html").unwrap();
-    //     let length = contents.len();
-
-    //     let response = format! {
-    //         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    //     };
-
-    //     stream.write_all(response.as_bytes()).unwrap();
-    // }
-
-    // 핸들링 코드 간결하게
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
 }
+
+// fn handle_connection(mut stream: TcpStream) {
+//     // TcpStream으로부터 데이터를 읽고 출력 기본 형태
+//     // let buf_reader = BufReader::new(&mut stream);
+//     // let http_request: Vec<_> = buf_reader
+//     //     .lines()
+//     //     .map(|result| result.unwrap())
+//     //     .take_while(|line| !line.is_empty())
+//     //     .collect();
+
+//     let buf_reader = BufReader::new(&mut stream);
+//     let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+//     // 여러 입력 핸들링 위함
+//     // if request_line == "GET / HTTP/1.1" {
+//     //     let status_line = "HTTP/1.1 200 OK";
+//     //     let contents = fs::read_to_string("hello.html").unwrap();
+//     //     let length = contents.len();
+
+//     //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+//     //     stream.write_all(response.as_bytes()).unwrap();
+//     // } else {
+//     //     let status_line = "HTTP/1.1 404 NOT FOUND";
+//     //     let contents = fs::read_to_string("404.html").unwrap();
+//     //     let length = contents.len();
+
+//     //     let response = format! {
+//     //         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+//     //     };
+
+//     //     stream.write_all(response.as_bytes()).unwrap();
+//     // }
+
+//     // 핸들링 코드 간결하게
+//     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+//         ("HTTP/1.1 200 OK", "hello.html")
+//     } else {
+//         ("HTTP/1.1 404 NOT FOUND", "404.html")
+//     };
+//     let contents = fs::read_to_string(filename).unwrap();
+//     let length = contents.len();
+
+//     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+//     stream.write_all(response.as_bytes()).unwrap();
+// }
